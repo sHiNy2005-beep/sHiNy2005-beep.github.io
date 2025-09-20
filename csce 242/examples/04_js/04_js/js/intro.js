d4a36691bfc50d6ca8c5a337eb5cd73e97f19aea
sHiNy2005-beep <RUCHIKA@email.sc.edu>
2025-09-20 11:23:24 -0400

    part4

--- csce 242/examples/04_js/04_js/js/intro.js
+++ csce 242/examples/04_js/04_js/js/intro.js
@@ -7,11 +7,43 @@ const sayHello = () => {
 document.getElementById("btn-click-me").onclick = sayHello;
 */
 document.getElementById("btn-click-me").onclick = (event) => {
-    document.getElementById("p-welcome").innerHTML ="Hello World.";
-    event.currentTarget.classList.add("clicked"); // current target is the button that was clicked.
+    document.getElementById("p-welcome").innerHTML = "Hello World";
+    event.currentTarget.classList.add("clicked");   //current target is the button that was clicked
 };
 
-document.getElementById("btn-happy").onclick =() => {
-    document.getElementById("p-feeling").onclick ="YYAY"
-    document.getElementById("p-feeling")
+document.getElementById("btn-happy").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "YAY";
+    pFeeling.classList.add("happy");
+    pFeeling.classList.remove("sad");
+}
+
+document.getElementById("btn-sad").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "Nay";
+    pFeeling.classList.add("sad");
+    pFeeling.classList.remove("happy");
+}
+
+document.getElementById("btn-clear").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "";
+    pFeeling.classList.remove("sad");
+    pFeeling.classList.remove("happy");
+}
+
+document.getElementById("txt-emotion").onkeyup = (event) => {
+    const userInput = event.currentTarget.value;
+    document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}.`
+    document.getElementById("img-emotion").classList.remove("hidden");
+};
+
+document.getElementById("btn-mood-ring").onclick= () =>{
+    const color = document.getElementById("txt-color").value;
+    const p = document.getElementById("p-mood-result");
+
+    if(color == ""){
+        document.getElementById("error-color").innerHTML ="* blank";
+    }
+
 }
