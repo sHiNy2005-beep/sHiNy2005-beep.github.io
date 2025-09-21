cff71abe9921c1980255bf94752f42c29e2122a2
sHiNy2005-beep <RUCHIKA@email.sc.edu>
2025-09-21 01:05:57 -0400

    part4

---
+++ csce 242/assignments/06conditionals/script.js
@@ -0,0 +1,68 @@
+
+// Exercise 1: PLANT
+const slider = document.getElementById("daysSlider");
+const output = document.getElementById("daysOutput");
+const message = document.getElementById("plantMessage");
+const image = document.getElementById("plantImage");
+
+slider.addEventListener("input", () => {
+  const days = parseInt(slider.value);
+  output.textContent = days;
+
+  const dayText = "It's been " + days + (days > 1 ? " days" : " day") + " since watering your plant.<br>";
+  let fullMessage = "";
+
+  if (days >= 1 && days <= 2) {
+    fullMessage = dayText + "Your plant is healthy and happy!";
+    image.src = "images/plantimage.png";
+    image.alt = "Healthyplant";
+  } else if (days >= 3 && days <= 5) {
+    fullMessage = dayText + "Your plant needs watering.";
+    image.src = "images/plantimage1.png";
+    image.alt = "plant";
+  } else if (days >= 6 && days <= 9) {
+    fullMessage = dayText + "Leaves are dropping, water soon.";
+    image.src = "images/plantimage2.png";
+    image.alt = "plant dying";
+  } else if (days >= 10 && days <= 12) {
+    fullMessage = dayText + "Sorry, your plant is no longer with us.";
+    image.src = "images/plantimage3.png";
+    image.alt = "Dead";
+  } else {
+    fullMessage = "";
+    image.src = "";
+    image.alt = "";
+  }
+
+  message.innerHTML = fullMessage;
+});
+
+// Exercise 2: clock
+function updateClock() {
+  const now = new Date();
+  let hours = now.getHours();
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12 || 12;
+  const timeString = "${hours}:${minutes} ${ampm"};
+  const clockDisplay = document.getElementById("clockDisplay");
+  if (clockDisplay) {
+    clockDisplay.textContent = timeString;
+  }
+
+setInterval(updateClock, 60000);
+updateClock();
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-items");
+  const toggle = document.querySelector(".menu-toggle");
+  menu.classList.toggle("show");
+  toggle.textContent = menu.classList.contains("show") ? "▲" : "▼";
+}
+
+function showExercise(num) {
+  const sections = document.querySelectorAll(".exercise-section");
+  sections.forEach(section => section.classList.add("hidden"));
+  const target = document.getElementById("exercise${num}");
+  if (target) target.classList.remove("hidden");
+}
---
+++ csce 242/examples/04_js/04_js/04_js/js/intro.js
@@ -0,0 +1,49 @@
+// console.log("Hello World"); this is to print on the console of a website when clicked on inspect on the righthand side of the page 
+
+/*
+const sayHello = () => {
+    console.log("Hello World");
+}
+document.getElementById("btn-click-me").onclick = sayHello;
+*/
+document.getElementById("btn-click-me").onclick = (event) => {
+    document.getElementById("p-welcome").innerHTML = "Hello World";
+    event.currentTarget.classList.add("clicked");   //current target is the button that was clicked
+};
+
+document.getElementById("btn-happy").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "YAY";
+    pFeeling.classList.add("happy");
+    pFeeling.classList.remove("sad");
+}
+
+document.getElementById("btn-sad").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "Nay";
+    pFeeling.classList.add("sad");
+    pFeeling.classList.remove("happy");
+}
+
+document.getElementById("btn-clear").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "";
+    pFeeling.classList.remove("sad");
+    pFeeling.classList.remove("happy");
+}
+
+document.getElementById("txt-emotion").onkeyup = (event) => {
+    const userInput = event.currentTarget.value;
+    document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}.`
+    document.getElementById("img-emotion").classList.remove("hidden");
+};
+
+document.getElementById("btn-mood-ring").onclick= () =>{
+    const color = document.getElementById("txt-color").value;
+    const p = document.getElementById("p-mood-result");
+
+    if(color == ""){
+        document.getElementById("error-color").innerHTML ="* blank";
+    }
+
+}
